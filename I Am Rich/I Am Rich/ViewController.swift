2dc0c14d15ef16cb9b183fd0d440ea8cbb42be7c
Bart Mars <bartmars@icloud.com>
2022-06-09 10:43:15 +0200

    added I am Rich and Poor apps

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Bart Mars on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
